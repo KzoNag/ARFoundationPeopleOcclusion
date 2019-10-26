28982c167cdb10f167b26418340962ad15b52c08
k_nagamine <k_nagamine@jollystics.com>
2019-10-26 22:12:20 +0900

    Add demo scene

---
+++ Assets/ARFoundationExtension/PeopleOcclusion/Demo/PlaceOnPlane.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+namespace ARFoundationExtension.PeopleOcclusion.Demo
+{
+	public class PlaceOnPlane : MonoBehaviour
+	{
+        [SerializeField]
+        private ARRaycastManager raycastManager;
+
+        [SerializeField]
+        private GameObject prefab;
+
+		void Update()
+		{
+            if(Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
+            {
+                var hits = new List<ARRaycastHit>();
+                if (raycastManager.Raycast(Input.touches[0].position, hits, TrackableType.Planes))
+                {
+                    Instantiate(prefab, hits[0].pose.position, Quaternion.identity);
+                }
+            }
+		}
+	}
+}
